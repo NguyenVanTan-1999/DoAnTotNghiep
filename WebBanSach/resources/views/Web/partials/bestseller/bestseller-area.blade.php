7c6108de5da29a6af02c2744b27a835abdb5446d
Tony Tan <nguyenvantan1611@gmail.com>
2020-08-11 18:53:51 +0700

    cat giam layout trang chu website p1

---
+++ WebBanSach/resources/views/Web/partials/bestseller/bestseller-area.blade.php
@@ -0,0 +1,149 @@
+<!-- bestseller-area-start -->
+<div class="bestseller-area pb-100">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+				<div class="bestseller-content">
+					<h1>Author best selling</h1>
+					<h2>J. K. <br />Rowling</h2>
+					<p class="categories">categories:<a href="#">Books</a> , <a href="#">Audiobooks</a></p>
+					<p>Vestibulum porttitor iaculis gravida. Praesent vestibulum varius placerat. Cras tempor congue neque, id aliquam orci finibus sit amet. Fusce at facilisis arcu. Donec aliquet nulla id turpis semper, a bibendum metus vulputate. Suspendisse potenti. </p>
+					<div class="social-author">
+						<ul>
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="banner-img-2">
+					<a href="#"><img src="{{ asset('assets/Web/img/banner/6.jpg') }}" alt="banner" /></a>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+				<div class="bestseller-active owl-carousel">
+					<div class="bestseller-total">
+						<div class="single-bestseller mb-25">
+							<div class="bestseller-img">
+								<a href="#"><img src="{{ asset('assets/Web/img/product/13.jpg') }}" alt="book" /></a>
+								<div class="product-flag">
+                                    <ul>
+                                        <li><span class="sale">new</span></li>
+                                        <li><span class="discount-percentage">-5%</span></li>
+                                    </ul>
+                                </div>
+							</div>
+							<div class="bestseller-text text-center">
+								<h3> <a href="#">Rival Messenger</a></h3>
+								<div class="price">
+									<ul>
+										<li><span class="new-price">$40.00</span></li>
+										<li><span class="old-price">$45.00</span></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="single-bestseller">
+							<div class="bestseller-img">
+								<a href="#"><img src="{{ asset('assets/Web/img/product/14.jpg') }}" alt="book" /></a>
+								<div class="product-flag">
+                                    <ul>
+                                        <li><span class="sale">new</span></li>
+                                    </ul>
+                                </div>
+							</div>
+							<div class="bestseller-text text-center">
+								<h3> <a href="#">Impulse Duffle</a></h3>
+								<div class="price">
+									<ul>
+										<li><span class="new-price">$70.00</span></li>
+										<li><span class="old-price">$74.00</span></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="bestseller-total">
+						<div class="single-bestseller mb-25">
+							<div class="bestseller-img">
+								<a href="#"><img src="{{ asset('assets/Web/img/product/15.jpg') }}" alt="book" /></a>
+								<div class="product-flag">
+                                    <ul>
+                                        <li><span class="sale">new</span></li>
+                                    </ul>
+                                </div>
+							</div>
+							<div class="bestseller-text text-center">
+								<h3> <a href="#">Voyage Yoga Bag</a></h3>
+								<div class="price">
+									<ul>
+										<li><span class="new-price">$30.00</span></li>
+										<li><span class="old-price">$32.00</span></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="single-bestseller">
+							<div class="bestseller-img">
+								<a href="#"><img src="{{ asset('assets/Web/img/product/16.jpg') }}" alt="book" /></a>
+								<div class="product-flag">
+                                    <ul>
+                                        <li><span class="sale">new</span></li>
+                                        <li><span class="discount-percentage">-5%</span></li>
+                                    </ul>
+                                </div>
+							</div>
+							<div class="bestseller-text text-center">
+								<h3> <a href="#">Compete Track Tote</a></h3>
+								<div class="price">
+									<ul>
+										<li><span class="new-price">$32.00</span></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="bestseller-total">
+						<div class="single-bestseller mb-25">
+							<div class="bestseller-img">
+								<a href="#"><img src="{{ asset('assets/Web/img/product/17.jpg') }}" alt="book" /></a>
+								<div class="product-flag">
+                                    <ul>
+                                        <li><span class="sale">new</span></li>
+                                    </ul>
+                                </div>
+							</div>
+							<div class="bestseller-text text-center">
+								<h3> <a href="#">Fusion Backpack</a></h3>
+								<div class="price">
+									<ul>
+										<li><span class="new-price">$59.00</span></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="single-bestseller">
+							<div class="bestseller-img">
+								<a href="#"><img src="{{ asset('assets/Web/img/product/14.jpg') }}" alt="book" /></a>
+								<div class="product-flag">
+                                    <ul>
+                                        <li><span class="sale">new</span></li>
+                                    </ul>
+                                </div>
+							</div>
+							<div class="bestseller-text text-center">
+								<h3> <a href="#">Impulse Duffle</a></h3>
+								<div class="price">
+									<ul>
+										<li><span class="new-price">$70.00</span></li>
+										<li><span class="old-price">$74.00</span></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bestseller-area-end -->
