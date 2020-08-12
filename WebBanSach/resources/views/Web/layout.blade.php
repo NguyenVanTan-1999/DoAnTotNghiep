affb0994ab3e06ec4e3912d5d970b4b75199e6ca
Tony Tan <nguyenvantan1611@gmail.com>
2020-08-12 14:59:50 +0700

    them duong dan chi tiet san pham trong icon gio hang ben website
    ok

--- WebBanSach/resources/views/Web/layout.blade.php
+++ WebBanSach/resources/views/Web/layout.blade.php
@@ -115,10 +115,10 @@
 												@foreach($product_cart as $product)
 													<div class="single-cart">
 														<div class="cart-img">
-															<a href="#"><img src="{{ asset('images/product/'.$product['item']['anh_minh_hoa_san_pham']) }}" alt="book" /></a>
+															<a href="{{ route('website-ban-sach.chi-tiet-san-pham', $product['item']['id']) }}"><img src="{{ asset('images/product/'.$product['item']['anh_minh_hoa_san_pham']) }}" alt="book" /></a>
 														</div>
 														<div class="cart-info">
-															<h5><a href="#">{{ $product['item']['ten_san_pham'] }}</a></h5>
+															<h5><a href="{{ route('website-ban-sach.chi-tiet-san-pham', $product['item']['id']) }}">{{ $product['item']['ten_san_pham'] }}</a></h5>
 															<p>{{$product['qty']}} x {{ number_format($product['item']['gia_tien_giam_gia'], 0, '', ',') }}</p>
 														</div>
 														<div class="cart-icon">
